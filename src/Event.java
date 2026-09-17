b79fe865e46dffb958d6d11ad81c653b136799d6
shagun25bai10074-oss <shagun.25bai10074@vitbhopal.ac.in>
2026-09-17 22:38:10 +0530

    Update Event.java

--- src/Event.java
+++ src/Event.java
@@ -1,1 +1,23 @@
+public class Event {
 
+    private int id;
+    private String name;
+    private String venue;
+    private String date;
+    private int capacity;
+
+    public Event(int id, String name, String venue, String date, int capacity) {
+        this.id = id;
+        this.name = name;
+        this.venue = venue;
+        this.date = date;
+        this.capacity = capacity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
